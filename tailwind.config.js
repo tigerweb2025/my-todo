eae152133c21801c1cb42e119856259ca5e11732
Brahim Madiri <tigerrweb@gmal.com>
2024-12-06 19:34:31 +0100

    4e config pour le deploiement

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,3 +1,4 @@
+import daisyui from 'daisyui'
 /** @type {import('tailwindcss').Config} */
 export default {
   content: [
@@ -9,7 +10,8 @@ export default {
   theme: {
     extend: {},
   },
-  plugins: [
-    require('daisyui')
-  ],
+  plugins: [daisyui],
+  daisyui: {
+    themes: ["light", "dark", "cupcake"],
+  }
 }
--- vite.config.js
+++ vite.config.js
@@ -1,25 +1,25 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+import path from 'path'
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  build: {
-    outDir: "dist",
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src'),
+    },
   },
   server: {
     port: 5173,
     proxy: {
       '/api': {
-        target: 'http://localhost:3000', // URL de votre JSON Server
+        target: 'http://localhost:3000',
         changeOrigin: true,
-        rewrite: (path) => path.replace(/^\/api/, ''),
+        rewrite: (path) => path.replace(/^\/api/, '')
       },
     },
   },
-  resolve: {
-    alias: {
-      "@": "/src",
-    },
-  },
-});
+  build: {
+    outDir: 'dist'
+  }
+})
