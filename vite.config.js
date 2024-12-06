06a18f4b761c6672e016f218a56aa3a2911bce30
Brahim Madiri <tigerrweb@gmal.com>
2024-12-06 17:09:45 +0100

    2e config vite,vercel files

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,5 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -9,10 +9,17 @@ export default defineConfig({
   },
   server: {
     port: 3000,
+    proxy: {
+      '/api': {
+        target: 'http://localhost:3000', // URL de votre JSON Server
+        changeOrigin: true,
+        rewrite: (path) => path.replace(/^\/api/, ''),
+      },
+    },
   },
   resolve: {
     alias: {
       "@": "/src",
     },
   },
-})
+});
